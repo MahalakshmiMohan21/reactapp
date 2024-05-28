50b41589295b2a670e5dfb778e896dde3a963ca2
MahalakshmiMohan21 <147132248+MahalakshmiMohan21@users.noreply.github.com>
2024-05-28 20:12:03 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>name:Mahalakshmi M</h1>
+            <h1>department: information technology</h1>
+            <h1>year: 3rd year</h1>
+           <h1> register number: 73772121157</h1>
+            <h1>area of interest: fullstack development</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
